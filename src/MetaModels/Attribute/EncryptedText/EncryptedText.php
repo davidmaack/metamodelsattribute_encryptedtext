2627bf75f8bfddbbd647e5d67a111681e8f177a3
David Maack <david.maack@arcor.de>
2016-02-03 19:16:42 +0100

    Save and restore the data via the encrpytion class when reading and writing data to the database

--- src/MetaModels/Attribute/EncryptedText/EncryptedText.php
+++ src/MetaModels/Attribute/EncryptedText/EncryptedText.php
@@ -17,6 +17,7 @@
 
 namespace MetaModels\Attribute\EncryptedText;
 
+use MetaModels\Attribute\IComplex;
 use MetaModels\Attribute\Text\Text;
 
 /**
@@ -26,7 +27,56 @@ use MetaModels\Attribute\Text\Text;
  * @subpackage AttributeText
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  */
-class EncryptedText extends Text
+class EncryptedText extends Text implements IComplex
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function setDataFor($arrValues)
+    {
+        $strTable   = $this->getMetaModel()->getTableName();
+        $strColName = $this->getColName();
+        foreach ($arrValues as $intId => $varData) {
+            $this->getMetaModel()->getServiceContainer()->getDatabase()
+                ->prepare(sprintf('UPDATE %s SET %s=? WHERE id=%s', $strTable, $strColName, $intId))
+                ->execute(\Encryption::encrypt($varData));
+        }
+    }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function getDataFor($arrIds)
+    {
+        $strTable   = $this->getMetaModel()->getTableName();
+        $strColName = $this->getColName();
+
+        $objDB = $this->getMetaModel()->getServiceContainer()->getDatabase();
+
+        $objValue = $this->getMetaModel()->getServiceContainer()->getDatabase()
+                ->prepare(sprintf('SELECT id, %s FROM %s WHERE id IN (' . $this->parameterMask($arrIds) . ')' , $strColName, $strTable))
+                ->execute($arrIds);
+
+        $arrReturn = array();
+        while ($objValue->next()) {
+            $arrReturn[$objValue->id] = \Encryption::decrypt($objValue->$strColName);
+        }
+
+        return $arrReturn;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function unsetDataFor($arrIds)
+    {
+        $strTable   = $this->getMetaModel()->getTableName();
+        $strColName = $this->getColName();
+
+        $objDB = $this->getMetaModel()->getServiceContainer()->getDatabase();
+
+        $objValue = $this->getMetaModel()->getServiceContainer()->getDatabase()
+                ->prepare(sprintf('DELETE FROM %s WHERE id IN (' . $this->parameterMask($arrIds) . ')' , $strTable))
+                ->execute($arrIds);
+    }
 }
